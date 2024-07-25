ce7ddd1e87877e2d4ed276728e963b49cf8c9126
liteoftheworld <kosuruth100@gmail.com>
2024-07-25 13:58:03 +0100

    wow

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
-import { GiLaptop } from "react-icons/gi";
 
 const AboutSection = () => {
   const [isPending, startTransition] = useTransition();
@@ -15,21 +14,26 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <GiLaptop style={{ fontSize: "300px" }} />
+        <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden flex-shrink-0">
+          <Image
+            src="/images/firstImg.jpg"
+            alt="hero image"
+            className="absolute inset-0 object-cover rounded-full"
+            layout="fill"
+          />
+        </div>
 
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+        <div className="mt-4 lg:mt-0 lg:ml-4 flex flex-col justify-center h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-sm leading-relaxed lg:text-lg">
             A frontend web developer with a passion for creating interactive and
             responsive web applications, experienced in JavaScript, React, HTML,
             CSS, and Git, always eager to expand my knowledge and collaborate
-            with others, with a love for exploring Nigeria&apos;s culture,
-            reading, music, and cooking. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            with others, with a love for exploring Nigeria's culture, reading,
+            music, and cooking. I am a quick learner and I am always looking to
+            expand my knowledge and skill set. I am a team player and I am
+            excited to work with others to create amazing applications.
           </p>
-
-          <div className="mt-8"></div>
         </div>
       </div>
     </section>
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { GiLaptop } from "react-icons/gi";
 
 const HeroSection = () => {
   return (
@@ -54,13 +55,8 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden">
-            <Image
-              src="/images/firstImg.jpg"
-              alt="hero image"
-              className="absolute inset-0 object-cover"
-              layout="fill"
-            />
+          <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+            <GiLaptop style={{ fontSize: "300px" }} />
           </div>
         </motion.div>
       </div>
